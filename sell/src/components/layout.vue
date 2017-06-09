12bd4c54bd4d9242e1c3b92124781f10b95c52f5
undarkness <407645831@qq.com>
2017-06-09 23:17:50 +0800

    首页
    左侧列表

--- sell/src/components/layout.vue
+++ sell/src/components/layout.vue
@@ -1,15 +1,43 @@
 <template>
-  <div class="app-header">header</div>
-  <div class="app-content">content</div>
-  <div class="app-footer">footer</div>  
+  <div>
+    <div class="app-head">
+      <div class="app-head-inner">
+        <div class="head-logo">
+          <img src="../assets/logo.png" alt="">
+        </div>
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>login</li>
+            <li class="nav-pile">|</li>
+            <li>register</li>
+            <li class="nav-pile">|</li>
+            <li>about</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="app-content">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive> 
+    </div>
+    <div class="app-foot">
+      <p>&copy; 2016 fishenal MIT</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
+
 export default {
-  name: 'app'
+  data () {
+    return {
+      msg: 'i am apple'
+    }
+  }
 }
 </script>
 
 <style>
+  @import './css/normal.css'
 </style>
--- sell/src/main.js
+++ sell/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Layout from '../conponents/layout'
+import Layout from './components/layout'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -10,6 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '</Layout>',
-  components: { App }
+  template: '<Layout/>',
+  components: { Layout }
 })
